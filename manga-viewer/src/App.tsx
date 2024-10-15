7994333c74ba7f983b072ccd98c6a5d448994025
Hemantadey02 <155042293+Hemantadey02@users.noreply.github.com>
2024-10-16 01:41:11 +0530

    Update App.tsx

--- manga-viewer/src/App.tsx
+++ manga-viewer/src/App.tsx
@@ -2,14 +2,40 @@ import React, { useState } from "react";
 import BookList from "./components/BookList";
 import ChapterList from "./components/ChapterList";
 import PageViewer from "./components/PageViewer";
+import { Chapter } from "./types";
 
 const App: React.FC = () => {
   const [selectedBook, setSelectedBook] = useState<number | null>(null);
+  const [chapters, setChapters] = useState<Chapter[]>([]);
   const [selectedChapter, setSelectedChapter] = useState<number | null>(null);
+  
+  const handleNextChapter = () => {
+    if (selectedChapter !== null) {
+      console.log(chapters);
+      const currentChapterIndex = chapters.findIndex(
+        (chapter) => chapter.id === selectedChapter
+      );
+      if (currentChapterIndex < chapters.length - 1) {
+        setSelectedChapter(chapters[currentChapterIndex + 1].id);
+      }
+    }
+  };
+  
+  const handlePreviousChapter = () => {
+    if (selectedChapter !== null) {
+      console.log(chapters);
+      const currentChapterIndex = chapters.findIndex(
+        (chapter) => chapter.id === selectedChapter
+      );
+      if (currentChapterIndex > 0) {
+        setSelectedChapter(chapters[currentChapterIndex - 1].id);
+      }
+    }
+  };
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center p-6">
-      {/* <h1 className="text-4xl text-center mb-8">Manga Reader</h1> */}
+    <div className="min-h-screen bg-gradient-to-b from-red-200 via-black to-cyan-200 flex flex-col items-center justify-center p-6">
+      <h1 className="text-4xl text-center mb-8">Manga Reader</h1>
       <BookList
         selectedBookId={selectedBook}
         onSelectBook={(bookId) => {
@@ -24,7 +50,16 @@ const App: React.FC = () => {
           onSelectChapter={setSelectedChapter}
         />
       )}
-      {selectedChapter && <PageViewer chapterId={selectedChapter} />}
+      {selectedChapter && (<PageViewer
+        chapterId={selectedChapter}
+        onNextChapter={handleNextChapter}
+        onPreviousChapter={handlePreviousChapter}
+        isLastChapter={
+          selectedChapter === chapters[chapters.length - 1]?.id
+        }
+        isFirstChapter={selectedChapter === chapters[0]?.id}
+      />
+      )}
     </div>
   );
 };
